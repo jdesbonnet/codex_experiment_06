a057a74682d985fdd6331b8afa0b7acb670c2e97
Joe Desbonnet <jdesbonnet@gmail.com>
2026-02-21 22:41:08 +0000

    sleep/wake C code working

--- common/include/lpc111x_min.h
+++ common/include/lpc111x_min.h
@@ -13,11 +13,18 @@
 #define LPC_GPIO1_BASE       0x50010000u
 #define LPC_UART_BASE        0x40008000u
 #define LPC_SSP0_BASE        0x40040000u
+#define LPC_PMU_BASE         0x40038000u
+#define LPC_CT32B0_BASE      0x40014000u
+#define LPC_CT16B0_BASE      0x4000C000u
 
 /* Cortex-M0 SysTick registers. */
 #define SYST_CSR            (*(volatile uint32_t *)(0xE000E010u))
 #define SYST_RVR            (*(volatile uint32_t *)(0xE000E014u))
 #define SYST_CVR            (*(volatile uint32_t *)(0xE000E018u))
+#define SCB_SCR             (*(volatile uint32_t *)(0xE000ED10u))
+#define NVIC_ISER0          (*(volatile uint32_t *)(0xE000E100u))
+#define NVIC_ICER0          (*(volatile uint32_t *)(0xE000E180u))
+#define NVIC_ICPR0          (*(volatile uint32_t *)(0xE000E280u))
 
 #define LPC_SYSCON_SYSAHBCLKCTRL   (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x080))
 #define LPC_SYSCON_SYSPLLCLKSEL    (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x040))
@@ -29,9 +36,20 @@
 #define LPC_SYSCON_SYSPLLCTRL      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x008))
 #define LPC_SYSCON_SYSPLLSTAT      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x00C))
 #define LPC_SYSCON_PRESETCTRL      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x004))
+#define LPC_SYSCON_WDTOSCCTRL      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x024))
+#define LPC_SYSCON_SYSRSTSTAT      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x030))
 #define LPC_SYSCON_PDRUNCFG        (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x238))
+#define LPC_SYSCON_PDSLEEPCFG      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x230))
+#define LPC_SYSCON_PDAWAKECFG      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x234))
 #define LPC_SYSCON_UARTCLKDIV      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x098))
 #define LPC_SYSCON_SSP0CLKDIV      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x094))
+#define LPC_SYSCON_WDTCLKSEL       (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x0D0))
+#define LPC_SYSCON_WDTCLKUEN       (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x0D4))
+#define LPC_SYSCON_WDTCLKDIV       (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x0D8))
+#define LPC_SYSCON_STARTAPRP0      (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x200))
+#define LPC_SYSCON_STARTERP0       (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x204))
+#define LPC_SYSCON_STARTRSRP0CLR   (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x208))
+#define LPC_SYSCON_STARTSRP0       (*(volatile uint32_t *)(LPC_SYSCON_BASE + 0x20C))
 
 #define LPC_GPIO0_DIR               (*(volatile uint32_t *)(LPC_GPIO_BASE + 0x8000))
 #define LPC_GPIO0_DATA              (*(volatile uint32_t *)(LPC_GPIO_BASE + 0x3FFC))
@@ -48,6 +66,8 @@
 
 /* IOCON register for PIO1_0 (address 0x4004 4078). */
 #define LPC_IOCON_PIO1_0            (*(volatile uint32_t *)(LPC_IOCON_BASE + 0x078))
+/* IOCON register for PIO0_1 (address 0x4004 4010). */
+#define LPC_IOCON_PIO0_1            (*(volatile uint32_t *)(LPC_IOCON_BASE + 0x010))
 
 /* IOCON registers for SPI0 pins. */
 #define LPC_IOCON_PIO0_2            (*(volatile uint32_t *)(LPC_IOCON_BASE + 0x01C))
@@ -74,5 +94,39 @@
 #define LPC_SSP0_DR                 (*(volatile uint32_t *)(LPC_SSP0_BASE + 0x008))
 #define LPC_SSP0_SR                 (*(volatile uint32_t *)(LPC_SSP0_BASE + 0x00C))
 #define LPC_SSP0_CPSR               (*(volatile uint32_t *)(LPC_SSP0_BASE + 0x010))
+
+/* CT32B0 timer registers. */
+#define LPC_CT32B0_IR               (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x000))
+#define LPC_CT32B0_TCR              (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x004))
+#define LPC_CT32B0_TC               (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x008))
+#define LPC_CT32B0_PR               (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x00C))
+#define LPC_CT32B0_PC               (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x010))
+#define LPC_CT32B0_MCR              (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x014))
+#define LPC_CT32B0_MR0              (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x018))
+#define LPC_CT32B0_MR2              (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x020))
+#define LPC_CT32B0_EMR              (*(volatile uint32_t *)(LPC_CT32B0_BASE + 0x03C))
+
+/* CT16B0 timer registers. */
+#define LPC_CT16B0_IR               (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x000))
+#define LPC_CT16B0_TCR              (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x004))
+#define LPC_CT16B0_TC               (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x008))
+#define LPC_CT16B0_PR               (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x00C))
+#define LPC_CT16B0_PC               (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x010))
+#define LPC_CT16B0_MCR              (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x014))
+#define LPC_CT16B0_MR0              (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x018))
+#define LPC_CT16B0_EMR              (*(volatile uint32_t *)(LPC_CT16B0_BASE + 0x03C))
+
+/* PMU registers. */
+#define LPC_PMU_PCON                (*(volatile uint32_t *)(LPC_PMU_BASE + 0x000))
+#define LPC_PMU_GPREG0              (*(volatile uint32_t *)(LPC_PMU_BASE + 0x004))
+
+/* Watchdog registers. */
+#define LPC_WDT_BASE                0x40004000u
+#define LPC_WDT_WDMOD               (*(volatile uint32_t *)(LPC_WDT_BASE + 0x000))
+#define LPC_WDT_WDTC                (*(volatile uint32_t *)(LPC_WDT_BASE + 0x004))
+#define LPC_WDT_WDFEED              (*(volatile uint32_t *)(LPC_WDT_BASE + 0x008))
+#define LPC_WDT_WDTV                (*(volatile uint32_t *)(LPC_WDT_BASE + 0x00C))
+#define LPC_WDT_WDWARNINT           (*(volatile uint32_t *)(LPC_WDT_BASE + 0x014))
+#define LPC_WDT_WDWINDOW            (*(volatile uint32_t *)(LPC_WDT_BASE + 0x018))
 
 #endif
--- common/src/startup.c
+++ common/src/startup.c
@@ -15,6 +15,26 @@ void HardFault_Handler(void)  __attribute__((weak, alias("Default_Handler")));
 void SVC_Handler(void)        __attribute__((weak, alias("Default_Handler")));
 void PendSV_Handler(void)     __attribute__((weak, alias("Default_Handler")));
 void SysTick_Handler(void)    __attribute__((weak, alias("Default_Handler")));
+void WAKEUP_IRQHandler(void)  __attribute__((weak, alias("Default_Handler")));
+void CAN_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void SSP1_IRQHandler(void)    __attribute__((weak, alias("Default_Handler")));
+void I2C_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void CT16B0_IRQHandler(void)  __attribute__((weak, alias("Default_Handler")));
+void CT16B1_IRQHandler(void)  __attribute__((weak, alias("Default_Handler")));
+void CT32B0_IRQHandler(void)  __attribute__((weak, alias("Default_Handler")));
+void CT32B1_IRQHandler(void)  __attribute__((weak, alias("Default_Handler")));
+void SSP0_IRQHandler(void)    __attribute__((weak, alias("Default_Handler")));
+void UART_IRQHandler(void)    __attribute__((weak, alias("Default_Handler")));
+void USB_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void USB_FIQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void ADC_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void WDT_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void BOD_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void FMC_IRQHandler(void)     __attribute__((weak, alias("Default_Handler")));
+void PIOINT3_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void PIOINT2_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void PIOINT1_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
+void PIOINT0_IRQHandler(void) __attribute__((weak, alias("Default_Handler")));
 
 int main(void);
 
@@ -36,6 +56,38 @@ void (* const g_pfnVectors[])(void) = {
     0,
     PendSV_Handler,
     SysTick_Handler,
+    WAKEUP_IRQHandler,  /* 16 PIO0_0 */
+    WAKEUP_IRQHandler,  /* 17 PIO0_1 */
+    WAKEUP_IRQHandler,  /* 18 PIO0_2 */
+    WAKEUP_IRQHandler,  /* 19 PIO0_3 */
+    WAKEUP_IRQHandler,  /* 20 PIO0_4 */
+    WAKEUP_IRQHandler,  /* 21 PIO0_5 */
+    WAKEUP_IRQHandler,  /* 22 PIO0_6 */
+    WAKEUP_IRQHandler,  /* 23 PIO0_7 */
+    WAKEUP_IRQHandler,  /* 24 PIO0_8 */
+    WAKEUP_IRQHandler,  /* 25 PIO0_9 */
+    WAKEUP_IRQHandler,  /* 26 PIO0_10 */
+    WAKEUP_IRQHandler,  /* 27 PIO0_11 */
+    WAKEUP_IRQHandler,  /* 28 PIO1_0 */
+    CAN_IRQHandler,     /* 29 */
+    SSP1_IRQHandler,    /* 30 */
+    I2C_IRQHandler,     /* 31 */
+    CT16B0_IRQHandler,  /* 32 */
+    CT16B1_IRQHandler,  /* 33 */
+    CT32B0_IRQHandler,  /* 34 */
+    CT32B1_IRQHandler,  /* 35 */
+    SSP0_IRQHandler,    /* 36 */
+    UART_IRQHandler,    /* 37 */
+    USB_IRQHandler,     /* 38 */
+    USB_FIQHandler,     /* 39 */
+    ADC_IRQHandler,     /* 40 */
+    WDT_IRQHandler,     /* 41 */
+    BOD_IRQHandler,     /* 42 */
+    FMC_IRQHandler,     /* 43 */
+    PIOINT3_IRQHandler, /* 44 */
+    PIOINT2_IRQHandler, /* 45 */
+    PIOINT1_IRQHandler, /* 46 */
+    PIOINT0_IRQHandler, /* 47 */
 };
 
 static void data_init(void)
